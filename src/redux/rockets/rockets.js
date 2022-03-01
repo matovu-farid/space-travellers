ae3ef9bb6c9e1267da8626f32711a1b43aed1023
matovu-farid <matovu90@gmail.com>
2022-03-01 11:15:53 +0300

    Add fetch logic

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,3 +1,24 @@
-const rocketsReducer = (state = []) => state;
+const FETCHDATA = 'space-travellers/rockets/fetch';
 
+export const fetchData = () => async (dispatch) => {
+  const url = 'https://api.spacexdata.com/v3/rocket';
+  const response = await fetch(url);
+  const data = await response.json();
+  const payload = data.map(({
+    id, rocket_name: name, description, flickr_images: images,
+  }) => ({
+    id, name, description, images,
+  }));
+  dispatch({
+    type: FETCHDATA,
+    payload,
+  });
+};
+
+const rocketsReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCHDATA: return action.payload;
+    default: return state;
+  }
+};
 export default rocketsReducer;
