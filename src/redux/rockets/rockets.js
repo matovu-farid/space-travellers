2e591d376e24004777661491b105b0414d4b9459
matovu-farid <matovu90@gmail.com>
2022-03-01 11:30:13 +0300

    Display the fetched rockets

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,13 +1,13 @@
 const FETCHDATA = 'space-travellers/rockets/fetch';
 
+const url = 'https://api.spacexdata.com/v3/rockets';
 export const fetchData = () => async (dispatch) => {
-  const url = 'https://api.spacexdata.com/v3/rocket';
   const response = await fetch(url);
   const data = await response.json();
   const payload = data.map(({
     id, rocket_name: name, description, flickr_images: images,
   }) => ({
-    id, name, description, images,
+    id, name, description, images, reserved: false,
   }));
   dispatch({
     type: FETCHDATA,
